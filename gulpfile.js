e0127a11ab0b58f8196b01169feb31a737b35393
Jon Daiello <jondaiello@gmail.com>
2016-04-05 22:00:48 -0400

    Setting up & getting started

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,44 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 gulp.task('js', function() {
   return gulp.src('./src/js/*.js')
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./assets/all.js'));
 
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./src/js/*.js')
     .pipe(sourcemaps.init())
     .pipe(js().on('error', js.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./assets/js'));
 });
-
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('js:watch', function () {
+  gulp.watch('./src/js/*.js', ['js']);
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
-
-  return gulp.src('./sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./assets/css'));
 });
-
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/*.scss', ['sass']);
 });
 
-gulp.task('js:watch', function () {
-  gulp.watch('./src/js/*.js', ['js']);
+gulp.task('img', function () {
+	return gulp.src('./src/img/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('./assets/img'));
 });
+gulp.task('img:watch', function () {
+  gulp.watch('./src/img/*', ['img']);
+});
+
+gulp.task('default', ['sass', 'js', 'img']);

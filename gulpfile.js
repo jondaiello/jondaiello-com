71731c8bcacee53cfcc1ed876d5bc03f46e27bd1
Jon Daiello <jondaiello@gmail.com>
2016-04-13 21:46:05 -0400

    more homepage

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,22 @@ var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var svgSprite = require('gulp-svg-sprite');
+//var cleanCSS = require('gulp-clean-css');
 
 gulp.task('js', function() {
   return gulp.src('./src/js/*.js')
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('./assets/all.js'));
-
-  return gulp.src('./src/js/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(js().on('error', js.logError))
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./assets/js'));
 });
 gulp.task('js:watch', function () {
   gulp.watch('./src/js/*.js', ['js']);
 });
 
+/*gulp.task('minify-css', function() {
+  return gulp.src('./assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./assets/css'));
+});*/
 gulp.task('sass', function () {
   return gulp.src('./src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))

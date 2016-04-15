23a3edb7c330e40ff97903f11e14d7c15e1bc5ac
Jon Daiello <jondaiello@gmail.com>
2016-04-15 13:19:34 -0400

    more homepage work. debugging main nav transition issue.

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,11 @@
 function init() {
   // funciton to control the header menu
-  /*var menuBtn = document.getElementById('menu-btn');
+  var menuBtn = document.getElementById('menu-btn');
   var headerNav = document.getElementById('header-nav');
   menuBtn.addEventListener('click', function(e) {
-    //e.preventDefault();
-    //headerNav.classList.toggle('open');
-    //headerNav.style = "display:block;height:auto;";
-  });*/
-
+    e.preventDefault();
+    headerNav.classList.toggle('open');
+  });
 }
 
 window.onload = init();

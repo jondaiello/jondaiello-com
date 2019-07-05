9e1f3ccc233a427fbebbfbd10fd5023d1f8c98e6
Jon Daiello <jondaiello@gmail.com>
2019-07-05 09:43:08 -0400

    tryng out nuxt

---
+++ jondaiello-com/pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Welcome! I'm Jon Daiello.
+      </h1>
+      <h2 class="subtitle">
+        UX Manager, Product Designer, and Front-End Developer
+      </h2>
+    </div>
+  </div>
+</template>
